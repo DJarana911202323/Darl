cf4afd330e2ee1705b9380bd249feb872230b172
micael__wh <142758143+DJarana911202323@users.noreply.github.com>
2024-10-07 11:58:23 -0300

    ark

--- perguntas.js
+++ perguntas.js
@@ -1,83 +1,83 @@
 CriaCartão(
-    ‘História’,
-    ‘Pergunta: Quem foi o primeiro presidente dos Estados Unidos?’,
-    ‘Resposta: George Washington’)
+    'História',
+    'Pergunta: Quem foi o primeiro presidente dos Estados Unidos?',
+    'Resposta: George Washington')
     
     CriaCartão(
-    ‘Geografia',
-    ‘Pergunta: Qual é a capital da França?,
-    ‘Resposta: Paris’)
+    'Geografia',
+    'Pergunta: Qual é a capital da França?,
+    'Resposta: Paris')
     CriaCartão(
-    ‘Ciência’,
-    ‘Pergunta: Qual é a fórmula química da água?’,
-    ‘Resposta: H2O’)
+    'Ciência',
+    'Pergunta: Qual é a fórmula química da água?',
+    'Resposta: H2O')
     CriaCartão(
-    ‘Literatura',
-    ‘Pergunta: Quem escreveu "Dom Quixote"?’,
-    ‘Resposta: Miguel de Cervantes’)
+    'Literatura',
+    'Pergunta: Quem escreveu "Dom Quixote"?',
+    'Resposta: Miguel de Cervantes')
     CriaCartão(
-    ‘Matemática',
-    ‘Pergunta: Qual é o valor de pi aproximadamente?’,
-    ‘Resposta: 3,14’)
+    'Matemática',
+    'Pergunta: Qual é o valor de pi aproximadamente?',
+    'Resposta: 3,14')
      CriaCartão(
     'Cinema',
-    ‘Pergunta: Quais são os três filmes da trilogia "O Senhor dos Anéis"?’,
-    ‘Resposta: A Sociedade do Anel, As Duas Torres, O Retorno do Rei’)
+    'Pergunta: Quais são os três filmes da trilogia "O Senhor dos Anéis"?',
+    'Resposta: A Sociedade do Anel, As Duas Torres, O Retorno do Rei')
     CriaCartão(
     'Música',
-    “Pergunta: Quem é conhecido como o "Rei do Pop"?’,
-    ‘Resposta: Michael Jackson’)
+    'Pergunta: Quem é conhecido como o "Rei do Pop'?',
+    'Resposta: Michael Jackson')
     CriaCartão(
-    ‘Tecnologia',
-    ‘Pergunta: O que é um computador?’,
-    ‘Resposta: Um dispositivo eletrônico que processa dados.’)
+    'Tecnologia',
+    'Pergunta: O que é um computador?',
+    'Resposta: Um dispositivo eletrônico que processa dados.')
     CriaCartão(
     'Esportes',
-    ‘Pergunta: Qual país venceu a Copa do Mundo de 2014?’,
-    ‘Resposta: Alemanha’)
+    'Pergunta: Qual país venceu a Copa do Mundo de 2014?',
+    'Resposta: Alemanha')
     CriaCartão(
-    'Biologia’,
-    ‘Pergunta: Qual é a função dos pulmões?’,
-    ‘Resposta: Trocar oxigênio e dióxido de carbono.’)
+    'Biologia',
+    'Pergunta: Qual é a função dos pulmões?',
+    'Resposta: Trocar oxigênio e dióxido de carbono.')
     CriaCartão(
-    ‘Artes’,
-    ‘Pergunta: Quem pintou a Mona Lisa?’,
-    ‘Resposta: Leonardo da Vinci’)
+    'Artes',
+    'Pergunta: Quem pintou a Mona Lisa?',
+    'Resposta: Leonardo da Vinci')
     CriaCartão(
-    ‘Física’,
-    ‘Pergunta: O que é a gravidade?’,
-    ‘Resposta: A força que atrai os objetos para o centro da Terra.’)
+    'Física',
+    'Pergunta: O que é a gravidade?',
+    'Resposta: A força que atrai os objetos para o centro da Terra.')
     CriarCartão(
-    ‘'Filosofia’,
-    ‘Pergunta: Quem é conhecido como o pai da filosofia ocidental?’,
-    ‘Resposta: Sócrates’)
+    ''Filosofia',
+    'Pergunta: Quem é conhecido como o pai da filosofia ocidental?',
+    'Resposta: Sócrates')
     CriarCartão(
-    ‘Mitologia',
-    ‘Pergunta: Quem é o deus do trovão na mitologia nórdica?’,
-    ‘Resposta: Thor’)
+    'Mitologia',
+    'Pergunta: Quem é o deus do trovão na mitologia nórdica?',
+    'Resposta: Thor')
     CriarCartão(
-    'Culinária’,
-    ‘Pergunta: Qual é o ingrediente principal da paella?’,
-    ‘Resposta: Arroz’)
+    'Culinária',
+    'Pergunta: Qual é o ingrediente principal da paella?',
+    'Resposta: Arroz')
     CriarCartão(
-    ‘Economia’,
-    ‘Pergunta: O que é inflação?’,
-    ‘Resposta: O aumento geral e contínuo dos preços.’)
+    'Economia',
+    'Pergunta: O que é inflação?',
+    'Resposta: O aumento geral e contínuo dos preços.')
     CriarCartão(
-    ‘Medicina’,
-    ‘Pergunta: Qual é a função do coração no corpo humano?’,
-    ‘Resposta: Bombear sangue para todo o corpo.’).
+    'Medicina',
+    'Pergunta: Qual é a função do coração no corpo humano?',
+    'Resposta: Bombear sangue para todo o corpo.').
     CriarCartão(
-    ‘Astronomia’,
-    ‘Pergunta: Qual é o planeta mais próximo do Sol?’,
-    ‘Resposta: Mercúrio’)
+    'Astronomia',
+    'Pergunta: Qual é o planeta mais próximo do Sol?',
+    'Resposta: Mercúrio')
     CriarCartão(
-    ‘Psicologia’,
-    ‘Pergunta: O que é a psicologia?’,
-    ‘Resposta: O estudo do comportamento e dos processos mentais.’)
+    'Psicologia',
+    'Pergunta: O que é a psicologia?',
+    'Resposta: O estudo do comportamento e dos processos mentais.')
     CriarCartão(
-    ‘Linguística’,
-    ‘Pergunta: O que é um idioma?’,
-    ‘Resposta: Um sistema de comunicação usado por uma comunidade.’)
+    'Linguística',
+    'Pergunta: O que é um idioma?',
+    'Resposta: Um sistema de comunicação usado por uma comunidade.')
     
     
