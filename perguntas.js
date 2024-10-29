fe72a4a6d78e631677516f7dbdc46bf88909be1b
micael__wh <142758143+DJarana911202323@users.noreply.github.com>
2024-10-29 14:51:48 -0300

    fdgf

--- perguntas.js
+++ perguntas.js
@@ -3,11 +3,11 @@ criarCartao(
     'Pergunta: Quem foi o primeiro presidente dos Estados Unidos?',
     'Resposta: George Washington')
     
-    criarCartão(
+    criarCartao(
     'Geografia',
     'Pergunta: Qual é a capital da França? ',
     'Resposta: Paris'),
-    criarCartão(
+    criarCartao(
     'Ciência',
     'Pergunta: Qual é a fórmula química da água?',
     'Resposta: H2O')
@@ -15,67 +15,67 @@ criarCartao(
     'Literatura',
     'Pergunta: Quem escreveu "Dom Quixote"?',
     'Resposta: Miguel de Cervantes')
-    criarCartão(
+    criarCartao(
     'Matemática',
     'Pergunta: Qual é o valor de pi aproximadamente?',
     'Resposta: 3,14')
-     criarCartão(
+     criarCartao(
     'Cinema',
     'Pergunta: Quais são os três filmes da trilogia "O Senhor dos Anéis"?',
     'Resposta: A Sociedade do Anel, As Duas Torres, O Retorno do Rei')
-    criarCartão(
+    criarCartao(
     'Música',
     'Pergunta: Quem é conhecido como o "Rei do Pop?',
     'Resposta: Michael Jackson')
-    criarCartão(
+    criarCartao(
     'Tecnologia',
     'Pergunta: O que é um computador?',
     'Resposta: Um dispositivo eletrônico que processa dados.')
-    criarCartão(
+    criarCartao(
     'Esportes',
     'Pergunta: Qual país venceu a Copa do Mundo de 2014?',
     'Resposta: Alemanha')
-    criarCartão(
+    criarCartao(
     'Biologia',
     'Pergunta: Qual é a função dos pulmões?',
     'Resposta: Trocar oxigênio e dióxido de carbono.')
-    criarCartão(
+    criarCartao(
     'Artes',
     'Pergunta: Quem pintou a Mona Lisa?',
     'Resposta: Leonardo da Vinci')
-    criarCartão(
+    criarCartao(
     'Física',
     'Pergunta: O que é a gravidade?',
     'Resposta: A força que atrai os objetos para o centro da Terra.')
-    criarCartão(
+    criarCartao(
     'Filosofia',
     'Pergunta: Quem é conhecido como o pai da filosofia ocidental?',
     'Resposta: Sócrates')
-    criarCartão(
+    criarCartao(
     'Mitologia',
     'Pergunta: Quem é o deus do trovão na mitologia nórdica?',
     'Resposta: Thor')
-    criarCartão(
+    criarCartao(
     'Culinária',
     'Pergunta: Qual é o ingrediente principal da paella?',
     'Resposta: Arroz')
-    criarCartão(
+    criarCartao(
     'Economia',
     'Pergunta: O que é inflação?',
     'Resposta: O aumento geral e contínuo dos preços.')
-    criarCartão(
+    criarCartao(
     'Medicina',
     'Pergunta: Qual é a função do coração no corpo humano?',
     'Resposta: Bombear sangue para todo o corpo.').
-    criarCartão(
+    criarCartao(
     'Astronomia',
     'Pergunta: Qual é o planeta mais próximo do Sol?',
     'Resposta: Mercúrio')
-    criarCartão(
+    criarCartao(
     'Psicologia',
     'Pergunta: O que é a psicologia?',
     'Resposta: O estudo do comportamento e dos processos mentais.')
-    criarCartão(
+    criarCartao(
     'Linguística',
     'Pergunta: O que é um idioma?',
     'Resposta: Um sistema de comunicação usado por uma comunidade.')
